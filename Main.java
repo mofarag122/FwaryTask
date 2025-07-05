be12e3f9701a8fec9a85b41aee4d5c35d8ce1b4e
Mohamed Ahmed Farag <mo.a.farag.122@gmail.com>
2025-07-05 05:39:46 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ public class Main{
         Product biscuits= new ExpiringShipProduct("Biscuits", 150, 2, LocalDate.of(2025, 9, 1), 0.7);
         Product tv= new ShippingProduct("TV", 5000, 3, 10);
         Product scratchCard= new Product("ScratchCard", 50, 10){};
-        // Product expirecheck = new ExpiringShipProduct("expirecheck", 150, 2, LocalDate.of(2024, 9, 1), 0.7);
+        //Product expirecheck = new ExpiringShipProduct("expirecheck", 150, 2, LocalDate.of(2024, 9, 1), 0.7);
         //Product quantitycheck = new ShippingProduct("quantitycheck", 450, 1, 0.3);
-         //Product balancecheck = new ShippingProduct("balancecheck", 50000, 3, 10.0);
+        //Product balancecheck = new ShippingProduct("balancecheck", 50000, 3, 10.0);
 
         Customer customer= new Customer("Mohamed", 20000);
         Cart cart= new Cart();
@@ -17,8 +17,8 @@ public class Main{
         cart.add(biscuits, 1);
         cart.add(tv, 3);
         cart.add(scratchCard, 1);
-       //  cart.add(expirecheck, 2);
-      // cart.add(quantitycheck,2);
+        //cart.add(expirecheck, 2);
+        //cart.add(quantitycheck,2);
         //cart.add(balancecheck, 2);
 
         CheckoutService.checkout(customer, cart);
